70bdc9bac185134b4537f943dc25408a59645194
tenzin <tenzin@nine.ch>
2021-08-18 10:00:22 +0200

    Make a basic User model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.7.3'
 
 gem 'rails', '~> 6.1.4'
+gem 'bcrypt', '3.1.13'
 gem 'sqlite3', '~> 1.4'
 gem 'bootstrap-sass', '3.4.1'
 gem 'puma', '~> 5.0'
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/moment', to: 'static_pages#moment'
   get '/contact', to: 'static_pages#contact'
-  get '/signup', to: 'user#new'
+  get '/signup', to: 'users#new'
 end

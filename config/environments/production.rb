c1ff6487242411b353f003e3513bf7f5220a4ce9
tenzin <tenzin@nine.ch>
2021-08-18 14:17:46 +0200

    Use SSL and the Puma webserver in prodctuion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+  config.force_ssl = false
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
@@ -117,4 +118,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
 end

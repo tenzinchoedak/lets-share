230776d771c5e88864bba8a67e0703a0359de262
tenzin <tenzin@nine.ch>
2021-08-31 14:11:39 +0200

    Finish user edit, update, index

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,7 +15,7 @@ module SessionsHelper
     def current_user
       if (user_id = session[:user_id])
         user = User.find_by(id: user_id)
-        if user && session[:session_token] == FILL_IN
+        if user && session[:session_token] == user.session_token
           @current_user = user
         end
       elsif (user_id = cookies.encrypted[:user_id])
@@ -26,6 +26,12 @@ module SessionsHelper
         end
       end
     end
+
+    # Returns true if the given user is the current user.
+    def current_user?(user)
+      user && user == @current_user
+    end
+
     # Returns true if the user is logged in, false otherwise.
     def logged_in?
       !current_user.nil?
@@ -42,5 +48,10 @@ module SessionsHelper
         reset_session
         @current_user = nil
     end
+
+    def store_location
+      session[:forwarding_url] = request.original_url if request.get?
+    end
+
 
 end
